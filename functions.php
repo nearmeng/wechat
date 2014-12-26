de1376e6e5a0f71df22ef95eddc9fdbbf6153968
nearmeng <nearmeng@qq.com>
2014-12-26 15:01:06 +0800

    file change

--- functions.php
+++ functions.php
@@ -3,8 +3,8 @@
 //*************************里面的内容根据自己实际情况自行修改**************************// 	
 	$_hostname	= 'localhost'; 
 	$_username	= 'root';    	//你的数据库的用户名
-	$_password	= '111111';		//你的数据库的密码
-	$_dbname	= "yourdb";	    //数据库的名称
+	$_password	= 'open';		//你的数据库的密码
+	$_dbname	= "wechat_db";	    //数据库的名称
 
 	//用户关注后的欢迎词，根据自己的微信公众账号修改
 	$WELCOME_MSG = "很高兴您使用本系统，我们将为您提供更好的信息服务，您可以输入?获取更多帮助";
@@ -21,9 +21,10 @@
 					<Content><![CDATA[%s]]></Content>
 					<FuncFlag>0</FuncFlag>
 					</xml>";  
+	// 连接dB
 	$conn = mysql_connect($_hostname, $_username, $_password);
 	mysql_select_db($_dbname, $conn);
-	mysql_query("set names utf-8");// connect to db
+	mysql_query("set names utf-8");
  
  	//自定义的方法都可以写到这个里面来
 	function show($keyword, $key, $fromUsername, $toUsername)
