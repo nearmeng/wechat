795e566c901d6709c11b041fcf4c41f71f5123b0
nearmeng <nearmeng@qq.com>
2015-01-06 21:24:42 +0800

    trace nanjing bus

--- db.sql
+++ db.sql
@@ -40,11 +40,12 @@ INSERT INTO `articles` (`id`,`title`,`description`,`picurl`,`url`,`type`,`keywor
 VALUES
 	(1,'welcome','欢迎大家访问hxknows 我们致力打造最好的服务。',NULL,NULL,'news','who','equals'),
 	(2,'help','您可以输入以下信息来获取我们的帮助：\nhelp  获取帮助\n知乎 当前知乎日报最热5条信息\n#write#记事内容 可以用来记录你当前想法\n #list# 列举出你的当前笔记记录\n#delete# 删除你的当前笔记记录','','','text','?','equals'),
-  (3,'zhihu','no',NULL,'http://news-at.zhihu.com/api/3/news/latest','extern_request','知乎','equals'),
+  (3,'zhihu','no use',NULL,'http://news-at.zhihu.com/api/3/news/latest','extern_request','知乎','equals'),
 	(4,'help','您可以输入以下信息来获取我们的帮助：\nhelp  获取帮助\n知乎 当前知乎日报最热5条信息\n#write#记事内容 可以用来记录你当前想法\n #list# 列举出你的当前笔记记录\n#delete# 删除你的当前笔记记录',NULL,NULL,'text','help','equals'),
-  (5,'write_notes','记录笔记成功!','','','text','#write#','startwith'),
-  (6,'list_notes','您的笔记如下：','','','text','#list#','startwith'),
-  (7,'remove_notes','您的笔记已经删除!','','','text','#delete#','startwith');
+  (5,'write_notes','记录笔记成功!','','','text','#记录','startwith'),
+  (6,'list_notes','您的笔记如下：','','','text','#列表','startwith'),
+  (7,'remove_notes','您的笔记已经删除!','','','text','#删除','startwith'),
+  (8,'trace_bus','no use','','','text','#跟踪','startwith');
 
 /*!40000 ALTER TABLE `articles` ENABLE KEYS */;
 UNLOCK TABLES;
--- user_command_funs.php
+++ user_command_funs.php
@@ -52,6 +52,7 @@
 		return $resultStr;
 	}
 
+	//删除日志
 	function remove_notes($keyword, $starwith_str, $description, $fromUsername, $toUsername)
 	{
 		global $textTpl;
@@ -62,6 +63,106 @@
 		mysql_query("delete from notes where uid='{$fromUsername}'");
 		//返回操作完成消息
 		$resultStr 	= sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+		return $resultStr;
+	}
+
+	//跟踪南京江宁公交车
+	function trace_bus($keyword, $starwith_str, $description, $fromUsername, $toUsername)
+	{
+		global $textTpl;
+		$time = time();
+		$msgType = 'text';
+		//1 取出相应的数据并检查
+		$trace_info = substr($keyword, strlen($starwith_str), strlen($keyword)-strlen($starwith_str));
+		$trace_info = trim($trace_info);
+		$row=split(' ', $trace_info);
+		//1.1 检查上下行
+		if($row[0] == "进城")
+		{
+			$up_or_down = 1;
+		}
+		else if($row[0] == "下乡")
+		{
+			$up_or_down = 2;
+		}
+		else
+		{
+			$contentStr = "你要进城还是下乡哪里哇，请重新输入！格式：#跟踪 上(下)行 公交名 所在站点";
+			$resultStr 	= sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+			return $resultStr;
+		}
+		//1.2 根据数据库返回line_id
+		$line_id_sql_result = mysql_query("select lineId from jnbus where lineName='{$row[1]}'");
+		$query_num = mysql_num_rows($line_id_sql_result);
+		if($query_num == 1)
+		{
+			$row1=mysql_fetch_row($line_id_sql_result);
+			$line_id = $row1[0];
+		}
+		else
+		{
+			$contentStr = "您输入的公交线路本系统没有收录，sorry";
+			$resultStr 	= sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+			return $resultStr;
+		}
+		if($row[2] == "")
+		{
+			$contentStr = "您所在的站点名为空，请重新输入！格式：#跟踪 上(下)行 公交名 所在站点";
+			$resultStr 	= sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+			return $resultStr;
+		}
+
+		//2 获得该条公交线的信息
+		$url_request = "http://112.2.33.3:5902/BusAndroid/android.do?command=toSta&lineId=".$line_id."&inDown=".($up_or_down==1?2:1); 
+        $url_response = file_get_contents($url_request);
+        $json_content = json_decode($url_response, true);
+        $station_num = json_content['total'];
+
+        foreach ($json_content['rows'] as $key => $item) 
+        {
+        	$station_name[$key] = $item['text']; 
+        }
+
+        $user_station_id = -1;
+        foreach ($station_name as $key => $value) {
+        	if($value == $row[3])
+        	{
+        		$user_station_id = $key;
+        	}
+        }
+        if($user_station_id == -1)
+        {
+        	$contentStr = "您输入的所在站点不存在，请重新输入！格式：#跟踪 上(下)行 公交名 所在站点";
+			$resultStr 	= sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+			return $resultStr;
+        }
+
+		//3 获得目前距离用户最近的公交站
+		$url_request = "http://112.2.33.3:5902/BusAndroid/android.do?command=toDis&lineId=".$line_id."&inDown=".$up_or_down."&stationNo=".$station_num; 
+        $url_response = file_get_contents($url_request);
+        $json_content = json_decode($url_response, true);
+
+        $best_bus = -1;
+        $min_distance = $station_num;
+
+        foreach ($json_content['rows'] as $item) {
+        	if(($item['stationNo']-$user_station_id) > 0 && ($item['stationNo']-$user_station_id) < $min_distance) 
+        	{
+        		$min_distance = ($item['stationNo']-$user_station_id);
+        		$best_bus = $item['carZbh'];
+        		$best_bus_station = $item['stationName'];
+        	}
+        }
+        if($best_bus == -1)
+        {
+        	$contentStr = "当前线路上没有合适的公交车";
+			$resultStr 	= sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+			return $resultStr;
+        }
+
+		//4 形成结果反馈给用户
+        $contentStr = "最近的公交车(".$best_bus.")正处于".$best_bus_station."\n 距离您".$min_distance."站,请耐心等候。";
+        $resultStr 	= sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
 		return $resultStr;
 	}
 
